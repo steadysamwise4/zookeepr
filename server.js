a4128a32c9b32e3ad2552694fe7147e6206a8166
Sam Davenport <samueldavenport@att.net>
2021-11-22 22:15:24 -0600

    front end data added to project in public directory. The server now serves the index.html page, but it doesn't function yet , although it looks as intended.

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({extended: true}));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -113,6 +115,10 @@ app.post('/api/animals', (req, res) => {
 
     res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
